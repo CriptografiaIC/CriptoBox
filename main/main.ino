5f4a11db88705a49bd7f17c4f00b673561e83a73
Plugner <48152341+Plugner@users.noreply.github.com>
2023-11-23 16:48:11 -0300

    Mais alterações para implementação completa

--- main/main.ino
+++ main/main.ino
@@ -26,6 +26,7 @@
 */
 
 short int currentStage = 0;
+short int page = 1;
 double p;
 double q;
 double msg;
@@ -37,23 +38,23 @@ void setup() {
   Serial.begin(9600);
   setupLCD();
   setupBtn();
-  if(currentStage == 0) startup();
+  if (currentStage == 0) startup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  if(isButtonPressed()) onButtonPress();
+
+  if (isButtonPressed()) onButtonPress();
   int pot = getCurrentPotentiometerPosition();
-  if(pot != lastPotPos) onPotentiometerValueChange(lastPotPos, pot);
-  
+  if (pot != lastPotPos) onPotentiometerValueChange(lastPotPos, pot);
+
   int x = digitalRead(BUTTON_PORT);
 }
 
 void log(String invoker, String action) {
-  Serial.println("<stage::"+String(currentStage)+"> [" + String(millis()) + "] " + invoker + ": " + action);
-  
-} 
+  Serial.println("<stage::" + String(currentStage) + "> [" + String(millis()) + "] " + invoker + ": " + action);
+
+}
 
 void setupBtn() {
   pinMode(BUTTON_PORT, INPUT_PULLUP);
@@ -64,30 +65,40 @@ bool isButtonPressed() {
 }
 
 void onButtonPress() {
-  log("onButtonPress","pressionado " + String(digitalRead(BUTTON_PORT) + " / curr: " + String(curr)));
-  if(currentStage == 0) { // Muda para o 1
+  log("onButtonPress", "pressionado " + String(digitalRead(BUTTON_PORT) + " / curr: " + String(curr)));
+  if (currentStage == 0) { // Muda para o 1
     currentStage = 1;
     clearLCD();
-    printLCD("Variavel p", 0,0);
-  } else if(curr == 0) return; else
-  if(currentStage == 1) { // Muda para o 2
+    printLCD("Variavel p", 0, 0);
+  } else if (curr == 0) return; else if (currentStage == 1) { // Muda para o 2
     currentStage = 2;
     p = curr; // Salva a variável
     curr = 0;
     clearLCD();
-    printLCD("Variavel q", 0,0);
-  } else if(curr == 0 ) return; else
-  if(currentStage == 2) {
+    printLCD("Variavel q", 0, 0);
+  } else if (curr == 0 ) return; else if (currentStage == 2) {
     currentStage = 3; // Prossegue com o jogo.
     q = curr; // Salva a variável
     curr = 0;
     clearLCD();
     printLCD("Mensagem", 0, 0);
-  } else if(curr == 0 ) return; else
-  if(currentStage == 3) {
-    currentStage = 4; // Prossegue com o jogo.
+  } else if (curr == 0 ) return; else if (currentStage == 3) {
+    // currentStage = 4; // Prossegue com o jogo.
     msg = curr; // Salva a variável
-    calcRSA(p,q,12);
+    double* rsa;
+    rsa = calcRSA(p, q, 12); // [p,q,c,d,e,msg]
+    clearLCD();
+    printLCD(String(page), 15, 0);
+    if (page == 1) {
+      printLCD("p=" + String(rsa[0]) + " q=" + String(rsa[1]), 0, 0);
+      printLCD("c=" + String(rsa[2]) + " d=" + String(rsa[3]), 0, 1);
+      page++;
+    } else if (page == 2) {
+      printLCD("e=" + String(rsa[4]), 0, 0);
+      printLCD("msg=" + String(rsa[5]), 0, 1);
+      page--;
+    }
+
   }
 }
 
@@ -103,8 +114,8 @@ int getCurrentPotentiometerPosition() {
 void onPotentiometerValueChange(int from, int to) {
   log("onPotentiometerValueChange", String(getCurrentPotentiometerPosition()));
   lastPotPos = to;
-  curr = primes[(int) (floor(to/93) >= 11 ? 10 : floor(to/93))];
-  if(currentStage == 1 || currentStage == 2 || currentStage == 3) refreshPotentiometerDisplay();
+  curr = primes[(int) (floor(to / 93) >= 11 ? 10 : floor(to / 93))];
+  if (currentStage == 1 || currentStage == 2 || currentStage == 3) refreshPotentiometerDisplay();
 }
 
 
@@ -112,19 +123,19 @@ void onPotentiometerValueChange(int from, int to) {
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 void setupLCD () {
-    log("setupLCD", "Inicializando LCD.");
-    lcd.begin(16, 2);
+  log("setupLCD", "Inicializando LCD.");
+  lcd.begin(16, 2);
 }
 
 void printLCD(const String message) {
-    log("printLCD", "Imprimiu '" + message + "'");
-    lcd.print(message);
+  log("printLCD", "Imprimiu '" + message + "'");
+  lcd.print(message);
 }
 
 void printLCD(const String message, short unsigned int x, short unsigned int y) {
-    log("printLCD", "Imprimiu '" + message + "' nas coordenadas " + x + "," + y);
-    lcd.setCursor(x,y);
-    lcd.print(message);
+  log("printLCD", "Imprimiu '" + message + "' nas coordenadas " + x + "," + y);
+  lcd.setCursor(x, y);
+  lcd.print(message);
 }
 
 void clearLCD() {
@@ -132,47 +143,39 @@ void clearLCD() {
 }
 
 void setLCDState(bool state) {
-  if(state) {
+  if (state) {
     log("setLCDState", "Desligou o LCD");
     lcd.noDisplay();
-  }else{
+  } else {
     log("setLCDState", "Ligou o LCD");
     lcd.display();
   }
 }
 
 void refreshPotentiometerDisplay() {
-  printLCD("Primo: " + String(curr) + "  ",0, 1);
+  printLCD("Primo: " + String(curr) + "  ", 0, 1);
 }
 
 void startup() {
   char b[] = "Aperte o botao";
   clearLCD();
-  printLCD("SESI CriptoBox",0,0);
-  printLCD(b,0,1);
+  printLCD("SESI CriptoBox", 0, 0);
+  printLCD(b, 0, 1);
 }
 
-int mmc (int a, int b) {
-  	int temp; //variável temporária
-	while (1) {
-		temp = a % b;
-		if (temp == 0)
-			return b;
-		a = b;
-		b = temp;
-	}
+double phi(double p, double q) {
+  return (p - 1) * (q - 1);
 }
-  
+
 // return: [p,q,c,d,e,msg]
 double* calcRSA(double p, double q, double msg) {
-   double e = 3;
-   
-   double phi_n = phi(p,q);
-   double d = fmod(1/e, phi_n); // d = e^-1 % φ(n)
-   double c = pow(msg, e); // c = msg^e
+  double e = 3;
 
-   // Then c^d should equals msg
-   double a[6] = {p,q,c,d,e,msg};
-   return a;
+  double phi_n = phi(p, q);
+  double d = fmod(1 / e, phi_n); // d = e^-1 % φ(n)
+  double c = pow(msg, e); // c = msg^e
+
+  // Then c^d should equals msg
+  double* a = new double[6] {p, q, c, d, e, msg};
+  return a;
 }
-  
